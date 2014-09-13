350a9ad21951bb31be190f5ba90fafb36b6d7110
Kira Bruneau <kira.bruneau@gmail.com>
2014-09-13 13:47:05 -0500

    - Modified xorlist to accept any generic pointer as a value - Added iterateList method - Added freeList method

--- xorlist.c
+++ xorlist.c
@@ -2,19 +2,21 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-void * ptrXor(void * left, void * right) {
-	return (void *)((uintptr_t)left ^ (uintptr_t)right);
-}
-
 typedef struct Node {
 	struct Node * mask;
-	int value;
+	void * value;
 } Node;
 
 typedef struct List {
 	struct Node * mask;
 	struct Node * head;
 } List;
+
+////////////////////////////////////////////////////////////////////////////////
+
+void * ptrXor(void * left, void * right) {
+	return (void *)((uintptr_t)left ^ (uintptr_t)right);
+}
 
 List * createList() {
 	List * list = malloc(sizeof(List));
@@ -23,42 +25,61 @@ List * createList() {
 	return list;
 }
 
-Node * createNode(Node * mask, int value) {
+Node * createNode(Node * mask, void * value) {
 	Node * node = malloc(sizeof(Node));
 	node->mask = mask;
 	node->value = value;
 	return node;
 }
 
-void prependList(List * list, int value) {
+void prependList(List * list, void * value) {
 	Node * head = list->head;
-	Node * node = createNode(ptrXor((Node *)list, head), value);
+	Node * node = createNode(ptrXor(list, head), value);
 
-	head->mask = ptrXor(node, ptrXor(head->mask, (Node *)list));
+	head->mask = ptrXor(node, ptrXor(head->mask, list));
 	list->mask = ptrXor(node, ptrXor(list->mask, head));
 	list->head = node;
 }
 
-void printList(List * list) {
+void iterateList(List * list, void (*callback)(void *)) {
 	Node * prev = (Node *)list;
 	Node * node = list->head;
 
 	while (node != (Node *)list) {
-		printf("%i\n", node->value);
+		callback(node->value);
 		Node * next = ptrXor(prev, node->mask);
 		prev = node;
 		node = next;
 	}
 }
 
+void freeList(List * list) {
+	Node * prev = (Node *)list;
+	Node * node = list->head;
+
+	while (node != (Node *)list) {
+		Node * next = ptrXor(prev, node->mask);
+		prev = node;
+		free(node);
+		node = next;
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+void printList(void * value) {
+	printf("%i\n", value);
+}
+
 int main() {
 	List * list = createList();
 
-	int i;
+	uintptr_t i;
 	for (i = 0; i < 100; i++) {
-		prependList(list, i);
+		prependList(list, (void *)i);
 	}
 
-	printList(list);
+	iterateList(list, printList);
+	freeList(list);
 	return 0;
 }
